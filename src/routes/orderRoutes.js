f463bdec199aadb8319e0f35cd12837a4b720826
Mert Acar <mertacarr56@gamil.com>
2026-04-05 14:29:48 +0300

    checkout-summary rotasi eklendi

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -1,9 +1,8 @@
-const express = require('express');
-const router = express.Router();
-const { createOrder, getUserOrders } = require('../controllers/orderController');
-const { protect } = require('../middlewares/authMiddleware');
-
-router.post('/', protect, createOrder);
-router.get('/', protect, getUserOrders);
-
-module.exports = router;
+const express = require('express'); 
+const router = express.Router(); 
+const { createOrder, getUserOrders, getCheckoutSummary } = require('../controllers/orderController'); 
+const { protect } = require('../middlewares/authMiddleware'); 
+router.get('/checkout-summary', protect, getCheckoutSummary); 
+router.get('/', protect, getUserOrders); 
+router.post('/', protect, createOrder); 
+module.exports = router; 
